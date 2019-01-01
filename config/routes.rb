ceea9913b21fd489c974de513885831b2beff769
zofiakochutek <zofiakochutek@gmail.com>
2019-01-01 17:15:59 +0100

    added ability to mark todo items as complete, and added a bit of styling to show the complete status

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  
+
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      member do
+       patch :complete
+      end
+    end
   end
 
   root "todo_lists#index"
   # dzięki temu na localhost:3000 od razu widok naszej aplikacji
   # bez tego potrzebna ścieżka to localhost:3000/todo_lists/index.html.erb
-
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
